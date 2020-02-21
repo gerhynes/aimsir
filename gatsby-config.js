85af0cedc0bf36a0d145ec5d963b0144f9bca022
Gerard Hynes <gkhynes@gmail.com>
2020-02-21 19:28:40 +0000

    Add styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `Ger Hynes`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
--- src/components/forecast.js
+++ src/components/forecast.js
@@ -1,27 +1,33 @@
 import React from "react"
+import styled from "styled-components"
+
+const ForecastList = styled.ul`
+display: "flex",
+flexWrap: "wrap",
+`
+
+const ForecastItem = styled.li`
+listStyle: "none",
+              background: "#EDF2F7",
+              margin: "1rem",
+              padding: "1rem",
+`
 
 export default function Forecast({ weather }) {
   return (
     <div>
       <h3>Three Day Forecast</h3>
-      <ul style={{ display: "flex", flexWrap: "wrap" }}>
+      <ForecastList>
         {weather.list.map(hour => (
-          <li
-            style={{
-              listStyle: "none",
-              background: "#EDF2F7",
-              margin: "1rem",
-              padding: "1rem",
-            }}
-          >
+          <ForecastItem>
             <h3>{hour.dt_txt}</h3>
             <h3>{`${Math.round(hour.main.temp)}°C`}</h3>
             <p>{`${hour.weather[0].description} ${Math.round(
               hour.main.temp_max
             )} / ${Math.round(hour.main.temp_min)}°C`}</p>
-          </li>
+          </ForecastItem>
         ))}
-      </ul>
+      </ForecastList>
     </div>
   )
 }
