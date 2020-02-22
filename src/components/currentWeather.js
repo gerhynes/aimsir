52e5fcfc39312d2f896dad25cbc5cf8d4bb0475f
Gerard Hynes <gkhynes@gmail.com>
2020-02-22 11:51:41 +0000

    Add wind and direction

--- src/components/currentWeather.js
+++ src/components/currentWeather.js
@@ -20,6 +20,33 @@ export default function CurrentWeather({ weather }) {
       <p>{`${weather.weather[0].main} ${Math.round(
         weather.main.temp_max
       )} / ${Math.round(weather.main.temp_min)}°C`}</p>
+      <span>
+        Wind {weather.wind.speed}
+        <span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            class="icon-location-pin"
+            style={{
+              width: "2rem",
+              transform: `rotate(${weather.wind.deg}deg)`,
+            }}
+          >
+            <g>
+              <path
+                class="secondary"
+                fill="#A0AEC0"
+                d="M12 1v6a3 3 0 0 0 0 6v9.31a1 1 0 0 1-.7-.29l-5.66-5.66A9 9 0 0 1 12 1z"
+              />
+              <path
+                class="primary"
+                fill="#A0AEC0"
+                d="M12 1a9 9 0 0 1 6.36 15.36l-5.65 5.66a1 1 0 0 1-.71.3V13a3 3 0 0 0 0-6V1z"
+              />
+            </g>
+          </svg>
+        </span>
+      </span>
     </WeatherTile>
   )
 }
--- src/components/forecast.js
+++ src/components/forecast.js
@@ -30,6 +30,33 @@ export default function Forecast({ weather }) {
             <p>{`${hour.weather[0].description} ${Math.round(
               hour.main.temp_max
             )} / ${Math.round(hour.main.temp_min)}°C`}</p>
+            <p>
+              Wind {hour.wind.speed}
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  class="icon-location-pin"
+                  style={{
+                    width: "2rem",
+                    transform: `rotate(${hour.wind.deg}deg)`,
+                  }}
+                >
+                  <g>
+                    <path
+                      class="secondary"
+                      fill="#A0AEC0"
+                      d="M12 1v6a3 3 0 0 0 0 6v9.31a1 1 0 0 1-.7-.29l-5.66-5.66A9 9 0 0 1 12 1z"
+                    />
+                    <path
+                      class="primary"
+                      fill="#A0AEC0"
+                      d="M12 1a9 9 0 0 1 6.36 15.36l-5.65 5.66a1 1 0 0 1-.71.3V13a3 3 0 0 0 0-6V1z"
+                    />
+                  </g>
+                </svg>
+              </span>
+            </p>
           </ForecastItem>
         ))}
       </ForecastList>
