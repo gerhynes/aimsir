c2a67b2bf2c68ab3e3c5d2f1b5719e73411c7941
Gerard Hynes <gkhynes@gmail.com>
2020-02-22 10:29:26 +0000

    Fix forecast styling

--- src/components/currentWeather.js
+++ src/components/currentWeather.js
@@ -1,13 +1,21 @@
 import React from "react"
+import styled from "styled-components"
+
+const WeatherTile = styled.div`
+  background: #edf2f7;
+  width: 250px;
+  padding: 1rem;
+  margin-bottom: 1rem;
+`
 
 export default function CurrentWeather({ weather }) {
   return (
-    <div style={{ background: "#EDF2F7", width: "250px", padding: "1rem" }}>
+    <WeatherTile>
       <h3>{weather.name}</h3>
       <h3>{`${Math.round(weather.main.temp)}°C`}</h3>
       <p>{`${weather.weather[0].main} ${Math.round(
         weather.main.temp_max
       )} / ${Math.round(weather.main.temp_min)}°C`}</p>
-    </div>
+    </WeatherTile>
   )
 }
--- src/components/forecast.js
+++ src/components/forecast.js
@@ -2,15 +2,16 @@ import React from "react"
 import styled from "styled-components"
 
 const ForecastList = styled.ul`
-display: "flex",
-flexWrap: "wrap",
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
 `
 
 const ForecastItem = styled.li`
-listStyle: "none",
-              background: "#EDF2F7",
-              margin: "1rem",
-              padding: "1rem",
+  list-style: none;
+  margin: 1rem;
+  padding: 1rem;
+  background: #edf2f7;
 `
 
 export default function Forecast({ weather }) {
@@ -19,7 +20,7 @@ export default function Forecast({ weather }) {
       <h3>Three Day Forecast</h3>
       <ForecastList>
         {weather.list.map(hour => (
-          <ForecastItem>
+          <ForecastItem key={hour.dt}>
             <h3>{hour.dt_txt}</h3>
             <h3>{`${Math.round(hour.main.temp)}°C`}</h3>
             <p>{`${hour.weather[0].description} ${Math.round(
